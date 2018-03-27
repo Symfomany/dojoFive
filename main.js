c01fb518fb52af325e6a603c797e2e87372a2c76
Anthony Troupenat <anthony.troupenat@gmail.com>
2018-03-27 12:38:04 +0200

    dojo 5

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const temperatureZero = () => {
   // your code here...
-  const tabFiltre = tableauTemp.filter(Temp => Temp < 100 && Temp > -100);
-  //console.log(tabFiltre);
-  const tabFiltrePositif = tabFiltre.map(Temp => Math.abs(Temp));
-  //console.log(tabFiltrePositif);
-  const tabFiltreZero = tabFiltrePositif.sort((TempA, TempB) => {
-    return TempA - TempB;
-  });
-  console.log(tabFiltre);
-  const moyenne = tabFiltre.reduce((a, b) => a + b, 0);
-  console.log(moyenne);
-};
+  const tabCent = tableauTemp.filter(temp => Math.abs(temp) < 100);
 
-let tableauTemp = [101, -101, 5, -5, 7.5, -44, 28];
+  const moyenne = tabCent.reduce((a, b) => a + b, 0);
+
+  let diff = Infinity;
+  const tempNearZero = tabCent.map(element => {
+    if(Math.abs(element) < diff){     
+      diff = Math.abs(element);
+    }
+    return diff;
+  }).reduce((a,b) => a=b,0);
+
+  console.log(tempNearZero);
+  console.log(moyenne);
+}
+
+
+let tableauTemp = [101, -101, 5, -5, 7.5, -44, 28,1000,4 ,3,-2, -1, -100];
 temperatureZero();
